0218b8ae860f226d809d73d0a381515da9dd3e0f
nico-palmar <nicolas.palmar8@gmail.com>
2025-10-05 17:50:05 -0400

    working vanilla pg

--- learning_rl/learning_rl/algorithms/vanilla_pg.py
+++ learning_rl/learning_rl/algorithms/vanilla_pg.py
@@ -12,9 +12,9 @@ get_action = lambda net, obs: get_probs(net, obs).sample().item()
 
 # watch the np.mean for categorical vs continous action spaces; depending on what log_probs returns
 # NOTE: watch sign on loss
-compute_policy_loss = lambda net, obs, actions, advantages: - np.mean(get_probs(net, obs).log_probs(actions) * advantages)
+compute_policy_loss = lambda net, obs, actions, advantages: - (get_probs(net, obs).log_prob(actions) * advantages).mean()
 
-compute_value_loss = lambda value_est, returns: np.mean(np.square(returns - value_est))
+compute_value_loss = lambda net, obs, returns: ((returns - net(obs)) ** 2).mean()
 
 def reset_episode(env, episode_rewards):
     # pass by reference; overwrite the list
@@ -23,11 +23,15 @@ def reset_episode(env, episode_rewards):
     return obs
 
 def reward_to_go(episode_rewards):
-    raise NotImplementedError("Have not implemented rtg yet")
+    rtg = [0] * len(episode_rewards)
+    for i in range(len(episode_rewards)-1, -1, -1):
+        if (i == len(episode_rewards) - 1):
+            rtg[i] = episode_rewards[i]
+        else:
+            rtg[i] = episode_rewards[i] + rtg[i+1]
+    return rtg
 
-
-
-def train(lr=1e-2, n_epochs = 20, batch_size=5000, env_name='CartPole-v0', render=True):
+def train(lr=1e-2, n_epochs = 20, batch_size=5000, env_name='CartPole-v1', render=True):
     # 1. Setup things global to entire training session
     # implement version of vanilla PG that works with discrete action spaces
     # TODO (npalmar): make version that works for both discrete and continuous action spaces
@@ -64,9 +68,10 @@ def train(lr=1e-2, n_epochs = 20, batch_size=5000, env_name='CartPole-v0', rende
             # sample for some trajectory
             epoch_observations.append(obs)
 
-            action = get_action(policy_net, obs)
+            torch_obs = torch.tensor(obs, dtype=torch.float32)
+            action = get_action(policy_net, torch_obs)
             obs, reward, done, _ = env.step(action)
-            value_estimates = value_func_net(obs)
+            value_estimates = value_func_net(torch_obs)
             # action and reward associated with observation at current time step
             episode_rewards.append(reward)
             epoch_actions.append(action)
@@ -99,18 +104,23 @@ def train(lr=1e-2, n_epochs = 20, batch_size=5000, env_name='CartPole-v0', rende
         epoch_advantage_function_vals = torch.tensor(epoch_advantage_function_vals, dtype=torch.float32)
         epoch_returns = torch.tensor(epoch_returns, dtype=torch.float32)
         epoch_value_estimates = torch.tensor(epoch_value_estimates, dtype=torch.float32)
+        episode_lengths = torch.tensor(episode_lengths, dtype=torch.int32)
         
         # Compute loss (gradient of expected reward), take a step
+        # print(epoch_observations.shape, epoch_actions.shape, epoch_advantage_function_vals.shape)
         policy_loss = compute_policy_loss(policy_net, epoch_observations, epoch_actions, epoch_advantage_function_vals)
         policy_optim.zero_grad()
         policy_loss.backward()
         policy_optim.step()
 
         # Compute updated advantage function estimate via loss, take a step
-        value_loss = compute_value_loss(epoch_value_estimates, epoch_returns)
+        value_loss = compute_value_loss(value_func_net, epoch_observations, epoch_returns)
         value_func_optim.zero_grad()
+        # print(policy_loss.shape, policy_loss)
         value_loss.backward()
         policy_optim.step()
+
+        print(f"Epoch {epoch}: Loss = {policy_loss.item():.3f} | Mean Return = {epoch_returns.mean().item():.3f} | Std Return = {epoch_returns.std().item():.3f} | Avg Ep Length = {episode_lengths.median()}")
 
     else:
         # close the environment on last for loop
